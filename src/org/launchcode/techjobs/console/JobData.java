3f143279ebfea4b1f56dcabdc0c90f7fafc7ddd6
Sandy Hadley <sandymariehadley@gmail.com>
2021-02-04 19:06:42 -0600

    Completed printjob

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -76,8 +77,28 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
+            }
+        }
+
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+            for (HashMap<String, String> job : allJobs) {
+                for (Map.Entry<String, String> columns : job.entrySet()) {
+                String aValue = columns.getValue();
+
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(job);
+                }
             }
         }
 
